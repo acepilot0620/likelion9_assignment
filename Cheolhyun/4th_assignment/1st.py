40f15b5b8ea5f1160a511239ba92c67e8acea5c9
CheolhyunPark <pch5445@gmail.com>
2021-05-16 15:05:54 +0900

    4주차 과제

--- Cheolhyun/4th_assignment/1st.py
+++ Cheolhyun/4th_assignment/1st.py
@@ -1,7 +1,7 @@
 a = int(input("숫자 입력 : "))
 b = 0
 for i in range(a):
-        print("\n")
+        print() # print("\n") 으로 쓰면 두 번 띄어짐. 파이썬은 print 자체에 줄넘김이 있어서
         b+=1
         for j in range(b):
             print("*", end='')
